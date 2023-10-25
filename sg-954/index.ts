27b6a43f75ad54ad4152bb764d805f9eb2ca8e03
Đào Hoàng Sơn <daohoangson@gmail.com>
2023-10-25 15:26:29 +0700

    Add support for `timeout` query param

--- sg-954/index.ts
+++ sg-954/index.ts
@@ -1,10 +1,17 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function (req: VercelRequest, res: VercelResponse) {
-  const shouldActSlow = req.query["slow"] === "1";
-  if (!shouldActSlow) {
+  const { js, timeout } = req.query;
+  if (typeof js !== "string") {
+    const jsTimeout = Math.max(1, Math.min(30, parseInt(timeout ?? "5")));
     res
       .setHeader("Content-Type", "text/html")
-      .send(`<body><script src="/sg-954?slow=1"></script>The SCRIPT src will timeout after 30s.</body>`);
+      .send(`<body><script src="/sg-954?js=${jsTimeout}"></script>The SCRIPT will load after ${jsTimeout}s.</body>`);
+  } else {
+    setTimeout(() => {
+      res
+        .setHeader("Content-Type", "application/javascript")
+        .send("console.log('Loaded!');");
+    }, parseInt(js) * 1000);
   }
 }
