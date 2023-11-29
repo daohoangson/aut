b837ff9eaef68fd13fdbcbf6e65a2ae96c9994f4
Đào Hoàng Sơn <daohoangson@gmail.com>
2023-11-29 10:52:27 +0700

    Add /facebook-iframe/

---
+++ facebook-iframe/index.ts
@@ -0,0 +1,32 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function (req: VercelRequest, res: VercelResponse) {
+  res
+    .setHeader("Content-Type", "text/html")
+    .send(`<p>
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+  </p>
+  
+  <iframe loading="lazy" style="border: none; overflow: hidden;" src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Felenaakrita%2Fposts%2Fpfbid02LsKGy5Af8KrZ1EPRg4AjPCLxMjN5sCinXDVaNpG95ovBavjYNgsS6u1ZvhdWUqjCl&amp;show_text=true&amp;width=500" width="500" height="170" frameborder="0" scrolling="no" allowfullscreen="allowfullscreen"></iframe>
+  
+  <p>
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+    Blah blah blah.
+  </p>
+  `);
+}
